8420a285f172e5609739bedb009150b37da4640d
PC-AM\antoi <antoine1995@live.ca>
2017-12-13 00:36:51 -0500

    Le token est prêt pour connexion et enregistrement dans Android.

--- Android/Konquest/app/src/main/java/ca/qc/cstj/konquest/Activity/EnregistrementActivity.kt
+++ Android/Konquest/app/src/main/java/ca/qc/cstj/konquest/Activity/EnregistrementActivity.kt
@@ -1,15 +1,77 @@
 package ca.qc.cstj.konquest.Activity
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ca.qc.cstj.konquest.R
+import ca.qc.cstj.konquest.helpers.ENREGISTREMENT_URL
+import ca.qc.cstj.konquest.helpers.TOKEN
+import ca.qc.cstj.konquest.helpers.TOKEN_INFORMATION
+import ca.qc.cstj.konquest.models.Explorateur
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpPost
+import kotlinx.android.synthetic.main.activity_enregistrement.*
+import org.json.JSONObject
 
 class EnregistrementActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enregistrement)
+
+        buttonEnregistrement.setOnClickListener {
+
+            var enregistrement = Explorateur(editTextCourrielEnregistrement.text.toString(),
+                                             editTextPseudonymeEnregistrement.text.toString(),
+                                             editTextMotDePasseEnregistrement.text.toString(),
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            null)
+
+            ENREGISTREMENT_URL.httpPost()
+            .header("Content-Type" to "application/json")
+            .body(enregistrement.toJson()).responseJson() { _, response, result ->
+                when(response.statusCode) {
+                    201 -> {
+                        editTextCourrielEnregistrement.text.clear()
+                        editTextPseudonymeEnregistrement.text.clear()
+                        editTextMotDePasseEnregistrement.text.clear()
+
+                        Toast.makeText(this,"Connexion en cours...", Toast.LENGTH_SHORT).show()
+
+                        // Mettre le token dans une variable.
+                        val jObject : JSONObject = result as JSONObject
+                        var token : String = jObject.getString("token")
+
+                        //Sauvegarder Token.
+                        val preferences_Token_Info : SharedPreferences? = this.getSharedPreferences(TOKEN_INFORMATION, 0)
+                        var editor_Token_Info : SharedPreferences.Editor? = preferences_Token_Info?.edit()
+                        editor_Token_Info?.putBoolean(TOKEN_INFORMATION,true)
+                        editor_Token_Info?.commit()
+
+                        val preferences_Token : SharedPreferences? = this.getSharedPreferences(TOKEN, 0)
+                        var editor_Token : SharedPreferences.Editor? = preferences_Token?.edit()
+                        editor_Token!!.putString(TOKEN,token)
+                        editor_Token?.commit()
+
+                        // On change d'activity.
+                        val intent = Intent(this@EnregistrementActivity,MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    404 -> {
+
+                    }
+                    500 -> {
+
+                    }
+
+                }
+                }
+            }
     }
 
     override fun onBackPressed() {
--- Android/Konquest/app/src/main/java/ca/qc/cstj/konquest/helpers/Services.kt
+++ Android/Konquest/app/src/main/java/ca/qc/cstj/konquest/helpers/Services.kt
@@ -1,7 +1,20 @@
 package ca.qc.cstj.konquest.helpers
 
+// Les URL :
+
 val BASE_URL = "https://andromia-maxroy1993.c9users.io/"
-
 val UNITS_URL = BASE_URL + "units/"
-
 val CONNEXION_URL = BASE_URL + "connexion"
+val ENREGISTREMENT_URL = BASE_URL + "enregistrement"
+
+// Token :
+val TOKEN = "Token"
+val TOKEN_INFORMATION = "Token_Information"
+
+/*
+* Référence Token :
+* https://stackoverflow.com/questions/9605913/how-to-parse-json-in-android
+* https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.js/-j-s-o-n/index.html
+* http://tutos-android-france.com/sharedpreferences/
+* https://openclassrooms.com/courses/creez-des-applications-pour-android/le-stockage-de-donnees-2
+*/
